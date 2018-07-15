78192fd387b4193577580df80c2b1ca140b54ea2
Klaus Hahn <klaus.hahn.work@gmail.com>
2018-07-15 16:40:28 +0200

    deleted heading, added objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,48 @@
-var formattedName = HTMLheaderName.replace("%data%", "Klaus A. Hahn");
-$("#header").prepend(formattedName);
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-$("#header").append(formattedRole);
+var bio = {
+	"name" : "Klaus Arruda Hahn",
+	"role": "Student",
+	"contacts": {
+		"mobile": "420 608 857 612",
+		"email": "klaus.hahn.work@gmail.com",
+		"github": "Klaus-hahn",
+		"location": "Prague"
+	},
+	"welcomeMessage": "hello, you are on my résumé"
+	"skills": ["Javascript", "HTML", "CSS", "Langages"],
+	"bioPic": "images/me.jpg"
+}
+
+var education = {
+    "schools": [
+		{
+	  		"name": "Leonardo Da Vinci",
+	  		"city": "Brasília"
+	  		"degree": "middle school",
+	  		"dates": 2013
+	  	},
+	  	{
+	  		"name": "Bom Jesus da Nossa Senhora do Rosário",
+	  		"city": "Paranaguá",
+	  		"degree": "middle school",
+	  		"dates": [ 2014, 2015, 2016 ]  		
+	  	},
+	  	{
+	  		"name": "Colégio Ideal",
+	  		"city": "Brasília",
+	  		"degree": "middle school",
+	  		"dates": 2017
+	  	}
+	],
+	"onlineCourses": [
+		{
+			"title": "Init to Javascript",
+			"school": "Udacity",
+			"dates": 2018
+		},
+		{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"dates": 2018
+		}
+	]
+}
